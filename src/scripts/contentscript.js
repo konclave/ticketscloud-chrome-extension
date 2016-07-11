3bd6bddb9cdf3f21f2881565fd8306ce9c01db9f
Ivan Vasilyev <vianomore@gmail.com>
2016-07-11 18:09:47 +0300

    extract SVG transformations to separate module

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,4 +1,4 @@
-const svg = document.querySelector('svg');
+import * as process from './process.js';
 
 function setSeatNumbers() {
   const seats = document.querySelectorAll('circle');
@@ -13,8 +13,8 @@ function setSeatNumbers() {
       text.setAttribute('font-weight', 'bold');
       text.setAttribute('class', 'temporary');
       seat.parentNode.insertBefore(text, seat.nextSibling);
-      text.setAttribute('x', seat.getAttribute('cx') * 1 - text.getBBox().width / 2);
-      text.setAttribute('y', seat.getAttribute('cy') * 1 + text.getBBox().height / 3);
+      text.setAttribute('x', (seat.getAttribute('cx') * 1) - (text.getBBox().width / 2));
+      text.setAttribute('y', (seat.getAttribute('cy') * 1) + (text.getBBox().height / 3));
     }
   });
 }
@@ -177,64 +177,12 @@ function rowsCompareRowNo(current, next) {
   }
 }
 
-function removeIdTags(svg) {
-  var sectorShapes = svg.querySelectorAll('[id*="sector_shape"]');
-  Array.prototype.forEach.call(sectorShapes, function(sector) {
-    //var id = sector.getAttribute('id').replace(/\s?sector_shape\s?/, '');
-    sector.setAttribute('fill', 'lightgrey');
-    //sector.setAttribute('id', id);
-  });
-}
-
 function postProcess(svg) {
-  flattenStyles(svg);
-  removeClasses(svg);
-  removeIdTags(svg);
-  sortNodes(svg);
+  process.flattenStyles(svg);
+  process.removeClasses(svg);
+  process.removeIdTags(svg);
+  process.sortNodes(svg);
 }
-
-window.chrome.runtime.onMessage.addListener(function(msg, sender, response) {
-  var seat;
-  var res = {};
-
-  if (msg.from === 'popup') {
-    seat = document.querySelectorAll('.active');
-    if (msg.subject.action === 'activeSeat') {
-      if (seat.length) {
-        res.row = seat.item(0).parentNode.getAttribute('tc-row-no');
-        res.sector = seat.item(0).parentNode.parentNode.getAttribute('tc-sector-name');
-        res.seat = seat.length > 1 ? null : seat.item(0).getAttribute('tc-seat-no');
-        res.range = seat.length;
-        response(res);
-      } else {
-        response(null);
-      }
-    } else if (msg.subject.seat || msg.subject.row || msg.subject.sector) {
-      if (msg.subject.seat && seat.length) {
-        if (/:/.test(msg.subject.seat)) {
-          changeRange(msg.subject.seat, msg.subject.row);
-        } else {
-          setSeatNo(seat.item(0), msg.subject.seat);
-        }
-      }
-
-      if (msg.subject.row) {
-        setRowNo(seat.item(0), msg.subject.row);
-      }
-
-      if (msg.subject.sector) {
-        setSectorTitle(seat.item(0), msg.subject.sector);
-      }
-
-    } else if (msg.subject.action === 'getSVG') {
-      var svg;
-      svg = document.querySelector('svg').cloneNode(true);
-      clearTemporaryNodes(svg);
-      postProcess(svg);
-      response(svg.outerHTML);
-    }
-  }
-});
 
 function printError() {
   var svg = document.querySelector('svg');
@@ -335,20 +283,16 @@ function isPlanCorrect(container) {
   }
 }
 
-function checkStructure() {
-  var container = svg.getElementById('plan-container');
-  var isOK;
-  var error;
-
-  isOK = isPlanCorrect(container);
+function checkStructure(svg) {
+  const container = svg.getElementById('plan-container');
+  const isOK = isPlanCorrect(container);
   if (isOK.error) {
-    error = isOK.error;
+    const error = isOK.error;
     delete isOK.error;
     printError(error, isOK);
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 
 function testSector(coord) {
@@ -395,366 +339,6 @@ function testSeat(coord, sectorIdx, rowIdx) {
   };
 }
 
-function flattenStyles(svg) {
-  var sectorNameFontSize;
-  var sectorFill;
-  var stroke;
-  var strokeWidth;
-  var fntRegExp;
-  var strokeRegExp;
-  var strokeWidthRegExp;
-  var styles;
-
-  for (var i = 0; i <= 100; i++) {
-    try {
-
-      fntRegExp = new RegExp('\\\.fnt' + i + '.+font-size:(\\d+)');
-      strokeRegExp = new RegExp('\\\.str' + i + '.+stroke:\\\s?((#.{6})|[A-Za-z]+)(;|\\\})');
-      strokeWidthRegExp = new RegExp('\\\.str' + i + '.+stroke-width:\\\s?(\\\d+)(;|\\\})');
-
-      styles = svg.getElementsByTagName('style')[0].innerHTML;
-
-      if (fntRegExp.test(styles)) {
-        sectorNameFontSize = styles.match(fntRegExp)[1];
-        Array.prototype.map.call(svg.querySelectorAll('.fnt' + i), setInlineFont(sectorNameFontSize));
-      }
-
-      if (strokeRegExp.test(styles)) {
-        stroke = styles.match(strokeRegExp)[1];
-        Array.prototype.map.call(svg.querySelectorAll('.str' + i), setInlineStroke(stroke));
-      }
-
-      if (strokeWidthRegExp.test(styles)) {
-        strokeWidth = styles.match(strokeWidthRegExp)[1];
-        Array.prototype.map.call(svg.querySelectorAll('.str' + i), setInlineStrokeWidth(strokeWidth));
-      }
-    } catch (e) {
-      window.console.log('not found style fnt' + i);
-    }
-  }
-}
-
-function removeClasses(svg) {
-  var seats = svg.querySelector('#plan-container').querySelectorAll('circle, path, polygon, rect');
-  if (seats) {
-    Array.prototype.forEach.call(seats, function(seat) {
-      seat.removeAttribute('class');
-    });
-  }
-}
-
-function setInlineFont(font) {
-  return function(element) {
-    element.setAttribute('font-family', 'Arial');
-    if (font) {
-      element.setAttribute('font-size', font);
-    }
-  };
-}
-
-function setInlineStroke(stroke) {
-  return function(element) {
-    if (stroke) {
-      element.setAttribute('stroke', stroke);
-    }
-  };
-}
-
-function setInlineStrokeWidth(strokeWidth) {
-  return function(element) {
-    if (strokeWidth) {
-      element.setAttribute('stroke-width', strokeWidth);
-    }
-  };
-}
-
-function cleanMeta() {
-  document.querySelector('svg').setAttribute('height', '100%');
-  document.querySelector('svg').setAttribute('width', '100%');
-  document.querySelector('svg').removeAttribute('xml:space');
-  document.querySelector('svg').removeAttribute('style');
-}
-
-function flattenTranslateTransform(row) {
-  var transform = row.getAttribute('transform');
-  var coords = {x: 0, y: 0};
-  var translateParams;
-
-  if (transform) {
-    translateParams = transform.match(/translate\((\d+\.?\d+?)\,? (\d+\.?\d+)?\)/);
-    if (translateParams) {
-      coords.x = translateParams[1];
-      coords.y = translateParams[2];
-    }
-  }
-
-  Array.prototype.map.call(row.querySelectorAll('circle'), function(seat) {
-    var x = seat.getAttribute('cx');
-    var y = seat.getAttribute('cy');
-
-    if (x) {
-      seat.setAttribute('cx', x * 1 + coords.x * 1);
-    }
-
-    if (y) {
-      seat.setAttribute('cy', y * 1 + coords.y * 1);
-    }
-  });
-
-  row.removeAttribute('transform');
-}
-
-function convertPathToCircle(path) {
-  var curve = path.getAttribute('d').substr(1).split(',');
-  var r = curve[1].split(' ')[1] * 1;
-  var x = curve[0].split(' ')[0] * 1;
-  var y;
-  if (r < 0) {
-    r = -r;
-    y = curve[0].split(' ')[1].split('c')[0] * 1 + r;
-  } else {
-    y = curve[0].split(' ')[1].split('c')[0] * 1 - r;
-  }
-
-  var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-  circle.setAttribute('cx', x);
-  circle.setAttribute('cy', y);
-  circle.setAttribute('r', r);
-
-  path.parentNode.replaceChild(circle, path);
-  return circle;
-}
-
-function cleanTransforms() {
-  var sectors = document.getElementById('plan-container').children;
-
-  Array.prototype.forEach.call(sectors, function(sector) {
-    var rows = sector.children;
-    flattenTranslateTransform(sector);
-    Array.prototype.forEach.call(rows, function(row) {
-      flattenTranslateTransform(row);
-    });
-  });
-}
-
-function convertPaths() {
-  var paths = document.getElementById('plan-container').querySelectorAll(':scope > g > g > path');
-  if (paths) {
-    Array.prototype.forEach.call(paths, convertPathToCircle);
-  }
-}
-
-function seatsCompare(current, next) {
-  var currentNo = current.getAttribute('tc-seat-no');
-  var nextNo = next.getAttribute('tc-seat-no');
-
-  if (currentNo && nextNo) {
-    if (parseInt(currentNo) > parseInt(nextNo)) {
-      return 11;
-    } else if (parseInt(currentNo) < parseInt(nextNo)) {
-      return -1;
-    } else {
-      return 0;
-    }
-  } else {
-    if (current.getAttribute('cx') * 1 > next.getAttribute('cx') * 1) {
-      return -1;
-    } else if (current.getAttribute('cx') * 1 < next.getAttribute('cx') * 1) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-}
-
-function addSeatAttributes(seat, idx) {
-  if (!seat.getAttribute('tc-seat-no')) {
-    seat.setAttribute('tc-seat-no', idx + 1);
-  }
-}
-
-function appendSortedNodes(nodes, parent, callback) {
-  var crlf;
-
-  if (!nodes.forEach) {
-    nodes = Array.prototype.slice.call(nodes);
-  }
-
-  nodes.forEach(function(node, idx) {
-    crlf = null;
-    if (callback && typeof callback === 'function') {
-      callback(node, idx);
-    }
-
-    if (node.nextSibling && node.nextSibling.nodeType === 3) {
-      crlf = node.nextSibling;
-    }
-
-    parent.appendChild(node);
-    if (crlf) {
-      parent.appendChild(crlf);
-    }
-  });
-}
-
-function sortSeats(row) {
-  var seats;
-  var arr;
-  var paths;
-  var circle;
-
-  seats = row.getElementsByTagName('circle');
-  if (!Array.prototype.slice.call(seats).length) {
-    paths = row.getElementsByTagName('path');
-    Array.prototype.forEach.call(paths, function(path) {
-      circle = convertPathToCircle(path);
-      path.parentNode.replaceChild(circle, path);
-    });
-  }
-
-  arr = Array.prototype.slice.call(seats);
-
-  arr.sort(seatsCompare);
-
-  appendSortedNodes(arr, row, addSeatAttributes);
-}
-
-function rowsCompare(current, next) {
-  var currentNo = current.getAttribute('tc-row-no');
-  var nextNo = next.getAttribute('tc-row-no');
-  var currentY = current.getElementsByTagName('circle')[Math.floor(current.getElementsByTagName('circle').length / 2)].getAttribute('cy');
-  var nextY = next.getElementsByTagName('circle')[Math.floor(next.getElementsByTagName('circle').length / 2)].getAttribute('cy');
-
-  if (currentNo && nextNo) {
-    if (parseInt(currentNo) > parseInt(nextNo)) {
-      return 1;
-    } else if (parseInt(currentNo) < parseInt(nextNo)) {
-      return -1;
-    } else {
-      return 0;
-    }
-  } else {
-    if (currentY * 1 > nextY * 1) {
-      return 1;
-    } else if (currentY * 1 < nextY * 1) {
-      return -1;
-    } else {
-      return 0;
-    }
-  }
-}
-
-function sortRows(rows) {
-  var arr = Array.prototype.slice.call(rows);
-  if (arr.length === 0) {
-    return;
-  }
-
-  var parent = rows[0].parentNode;
-
-  arr.sort(rowsCompare);
-  appendSortedNodes(arr, parent, function(row, idx) {
-    if (!row.getAttribute('tc-row-no')) {
-      row.setAttribute('tc-row-no', idx + 1);
-    }
-  });
-}
-
-function sortNodes(svg) {
-  var sectors = svg.getElementById('plan-container').children;
-
-  Array.prototype.forEach.call(sectors, function(sector) {
-    var rows = sector.querySelectorAll('g');
-
-    Array.prototype.forEach.call(rows, function(row) {
-      sortSeats(row);
-    });
-
-    sortRows(rows);
-  });
-}
-
-function setSectorName() {
-  var sectors = document.getElementById('plan-container').children;
-
-  Array.prototype.forEach.call(sectors, function(sector) {
-    var sectorId = sector.getAttribute('id');
-
-    if (sector.getAttribute('tc-sector-name')) {
-      return;
-    }
-
-    if (sectorId) {
-      sectorId = sectorId.replace(/_(x(\d|[a|b|c|d|e]){4})_/g, function() {
-        return String.fromCharCode('0' + arguments[1]);
-      }).replace('-', ' ').trim();
-      sector.setAttribute('tc-sector-name', sectorId);
-    }
-  });
-}
-
-function wrapChildrenWithGroup(node) {
-  var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-  for (var i = 0, j = node.children.length; i < j; i++) {
-    g.appendChild(node.children[0]);
-  }
-
-  //Array.prototype.map.call(node.children, function(seat) {
-  //  g.appendChild(seat);
-  //});
-
-  node.appendChild(g);
-  return g;
-}
-
-function wrapNodeWithGroup(node) {
-  var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-  node.parentNode.insertBefore(g, node);
-  g.appendChild(node);
-  return g;
-}
-
-function wrapSingleGroups() {
-  var planContainer = document.getElementById('plan-container');
-  var wrapRow;
-  var rowWithoutSector;
-  var seatsWithoutRow;
-  var id;
-
-  if (!planContainer) return;
-
-  wrapRow = planContainer.querySelectorAll('[id*="wrap_rows"]');
-  rowWithoutSector = planContainer.querySelectorAll('[id*="add_sector"]');
-  seatsWithoutRow = planContainer.querySelectorAll('[id*="add_row"]');
-
-  if (rowWithoutSector.length) {
-    Array.prototype.forEach.call(rowWithoutSector, function(row) {
-      id = row.getAttribute('id').replace(/\s?add_sector\s?/, '');
-      row.removeAttribute('id');
-      if (/\s?sector_shape\s?/.test(id)) {
-        row.setAttribute('id', 'sector_shape');
-      }
-
-      id = id.replace(/\s?sector_shape\s?/, '');
-      wrapNodeWithGroup(row).setAttribute('id', id);
-    });
-  }
-
-  if (seatsWithoutRow.length) {
-    Array.prototype.forEach.call(seatsWithoutRow, function(seat) {
-      seat.removeAttribute('id');
-      wrapNodeWithGroup(seat);
-    });
-  }
-
-  if (wrapRow.length) {
-    var title = wrapRow[0].getAttribute('id');
-    wrapRow[0].removeAttribute('id');
-    title = title.replace(/\s?wrap_rows\s?/, '');
-    wrapChildrenWithGroup(wrapRow[0].parentNode).setAttribute('id', title);
-  }
-}
-
 function applyTranslate(element, translate) {
   switch (element.tagName) {
     case 'circle':
@@ -777,59 +361,84 @@ function extractChildren(element) {
   element.parentNode.removeChild(element);
 }
 
-function flattenTransform(element) {
-  var transform = element.getAttribute('transform');
-
-  if (transform) {
-    var translate = transform.match(/translate\((\d+\.?\d+?)\)/);
-    if (element.tagName === 'g') {
-      Array.prototype.forEach.call(element.children, function(child) {
-        applyTranslate(child, {x: translate[1]});
-      });
-
-      extractChildren(element);
-    }
-  }
-}
-
-function flattenTransforms(svg) {
-  var groups = svg.querySelectorAll('g[transform]');
-  Array.prototype.forEach.call(groups, function(group) {
-    flattenTransform(group);
-  });
-}
-
 function preprocess() {
-  var svg = document.querySelector('svg');
+  const svg = document.querySelector('svg');
 
-  cleanMeta();
-  wrapSingleGroups();
-  // flattenTransforms(svg);
+  process.cleanMeta(svg);
+  process.wrapSingleGroups(svg);
 
-  if (!checkStructure()) {
+  if (!checkStructure(svg)) {
     return false;
   }
 
-  cleanTransforms();
-  convertPaths();
-  sortNodes(svg);
-  setSectorName();
-  flattenStyles(svg);
+  process.cleanTransforms(svg);
+  process.convertPaths(svg);
+  process.sortNodes(svg);
+  process.setSectorName(svg);
+  process.flattenStyles(svg);
 
   return true;
 }
 
-if (svg) {
+function onMessageCallback(msg, sender, response) {
+  const res = {};
+
+  if (msg.from === 'popup') {
+    const seat = document.querySelectorAll('.active');
+    if (msg.subject.action === 'activeSeat') {
+      if (seat.length) {
+        res.row = seat.item(0).parentNode.getAttribute('tc-row-no');
+        res.sector = seat.item(0).parentNode.parentNode.getAttribute('tc-sector-name');
+        res.seat = seat.length > 1 ? null : seat.item(0).getAttribute('tc-seat-no');
+        res.range = seat.length;
+        response(res);
+      } else {
+        response(null);
+      }
+    } else if (msg.subject.seat || msg.subject.row || msg.subject.sector) {
+      if (msg.subject.seat && seat.length) {
+        if (/:/.test(msg.subject.seat)) {
+          changeRange(msg.subject.seat, msg.subject.row);
+        } else {
+          setSeatNo(seat.item(0), msg.subject.seat);
+        }
+      }
+
+      if (msg.subject.row) {
+        setRowNo(seat.item(0), msg.subject.row);
+      }
+
+      if (msg.subject.sector) {
+        setSectorTitle(seat.item(0), msg.subject.sector);
+      }
+    } else if (msg.subject.action === 'getSVG') {
+      const svg = document.querySelector('svg').cloneNode(true);
+      clearTemporaryNodes(svg);
+      postProcess(svg);
+      response(svg.outerHTML);
+    }
+  }
+}
+
+function init() {
+  const svg = document.querySelector('svg');
+
+  if (!svg) return;
   if (preprocess()) {
     setSeatNumbers();
     setRowNumbers();
     attachEvents();
   }
 
-  var html = document.createElementNS('http://www.w3.org/1999/xhtml', 'html');
-  var body = document.createElementNS('http://www.w3.org/1999/xhtml', 'body');
+  const html = document.createElementNS('http://www.w3.org/1999/xhtml', 'html');
+  const body = document.createElementNS('http://www.w3.org/1999/xhtml', 'body');
+
   html.appendChild(document.createElementNS('http://www.w3.org/1999/xhtml', 'head'));
   html.appendChild(body);
   body.appendChild(svg);
   document.appendChild(html);
+
+  window.chrome.runtime.onMessage.addListener(onMessageCallback);
 }
+
+init();
---
+++ src/scripts/process.js
@@ -0,0 +1,391 @@
+/**
+ * removes unused attributes from SVG
+ * @param svg {Element}
+ */
+function cleanMeta(svg) {
+  svg.setAttribute('height', '100%');
+  svg.setAttribute('width', '100%');
+  svg.removeAttribute('xml:space');
+  svg.removeAttribute('style');
+}
+
+function flattenStyles(svg) {
+  var sectorNameFontSize;
+  var stroke;
+  var strokeWidth;
+  var fntRegExp;
+  var strokeRegExp;
+  var strokeWidthRegExp;
+  var styles;
+
+  for (var i = 0; i <= 100; i++) {
+    try {
+      fntRegExp = new RegExp('\\\.fnt' + i + '.+font-size:(\\d+)');
+      strokeRegExp = new RegExp('\\\.str' + i + '.+stroke:\\\s?((#.{6})|[A-Za-z]+)(;|\\\})');
+      strokeWidthRegExp = new RegExp('\\\.str' + i + '.+stroke-width:\\\s?(\\\d+)(;|\\\})');
+
+      styles = svg.getElementsByTagName('style')[0].innerHTML;
+
+      if (fntRegExp.test(styles)) {
+        sectorNameFontSize = styles.match(fntRegExp)[1];
+        Array.prototype.map.call(svg.querySelectorAll('.fnt' + i), setInlineFont(sectorNameFontSize));
+      }
+
+      if (strokeRegExp.test(styles)) {
+        stroke = styles.match(strokeRegExp)[1];
+        Array.prototype.map.call(svg.querySelectorAll('.str' + i), setInlineStroke(stroke));
+      }
+
+      if (strokeWidthRegExp.test(styles)) {
+        strokeWidth = styles.match(strokeWidthRegExp)[1];
+        Array.prototype.map.call(svg.querySelectorAll('.str' + i), setInlineStrokeWidth(strokeWidth));
+      }
+    } catch (e) {
+      window.console.log('not found style fnt' + i);
+    }
+  }
+}
+
+function wrapChildrenWithGroup(node) {
+  var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+  for (var i = 0, j = node.children.length; i < j; i++) {
+    g.appendChild(node.children[0]);
+  }
+
+  // Array.prototype.map.call(node.children, function(seat) {
+  //   g.appendChild(seat);
+  // });
+
+  node.appendChild(g);
+  return g;
+}
+
+function wrapNodeWithGroup(node) {
+  var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+  node.parentNode.insertBefore(g, node);
+  g.appendChild(node);
+  return g;
+}
+
+function wrapSingleGroups() {
+  var planContainer = document.getElementById('plan-container');
+  var wrapRow;
+  var rowWithoutSector;
+  var seatsWithoutRow;
+  var id;
+
+  if (!planContainer) return;
+
+  wrapRow = planContainer.querySelectorAll('[id*="wrap_rows"]');
+  rowWithoutSector = planContainer.querySelectorAll('[id*="add_sector"]');
+  seatsWithoutRow = planContainer.querySelectorAll('[id*="add_row"]');
+
+  if (rowWithoutSector.length) {
+    Array.prototype.forEach.call(rowWithoutSector, function(row) {
+      id = row.getAttribute('id').replace(/\s?add_sector\s?/, '');
+      row.removeAttribute('id');
+      if (/\s?sector_shape\s?/.test(id)) {
+        row.setAttribute('id', 'sector_shape');
+      }
+
+      id = id.replace(/\s?sector_shape\s?/, '');
+      wrapNodeWithGroup(row).setAttribute('id', id);
+    });
+  }
+
+  if (seatsWithoutRow.length) {
+    Array.prototype.forEach.call(seatsWithoutRow, function(seat) {
+      seat.removeAttribute('id');
+      wrapNodeWithGroup(seat);
+    });
+  }
+
+  if (wrapRow.length) {
+    var title = wrapRow[0].getAttribute('id');
+    wrapRow[0].removeAttribute('id');
+    title = title.replace(/\s?wrap_rows\s?/, '');
+    wrapChildrenWithGroup(wrapRow[0].parentNode).setAttribute('id', title);
+  }
+}
+
+function cleanTransforms(svg) {
+  const sectors = svg.getElementById('plan-container').children;
+
+  Array.prototype.forEach.call(sectors, function(sector) {
+    const rows = sector.children;
+    flattenTranslateTransform(sector);
+    Array.prototype.forEach.call(rows, function(row) {
+      flattenTranslateTransform(row);
+    });
+  });
+}
+
+/**
+ * Converts circle-like seat paths to circles
+ * inside group inside group inside #plan-container element
+ * @param svg {Element}
+ */
+function convertPaths(svg) {
+  const paths = svg.querySelectorAll('#plan-container > g > g > path');
+  if (paths) {
+    Array.prototype.forEach.call(paths, convertPathToCircle);
+  }
+}
+
+/**
+ * Converts circle-like path to circle shape
+ * @param path
+ * @returns {Element}
+ */
+function convertPathToCircle(path) {
+  const params = getCircleParamsFromPath(path);
+  const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+
+  circle.setAttribute('cx', params.x);
+  circle.setAttribute('cy', params.y);
+  circle.setAttribute('r', params.r);
+
+  path.parentNode.replaceChild(circle, path);
+  return circle;
+}
+
+function getCircleParamsFromPath(path) {
+  const curve = path.getAttribute('d').substr(1).split(',');
+  const x = curve[0].split(' ')[0] * 1;
+  let r = curve[1].split(' ')[1] * 1;
+  let y;
+
+  if (r < 0) {
+    r = -r;
+    y = (curve[0].split(' ')[1].split('c')[0] * 1) + r;
+  } else {
+    y = (curve[0].split(' ')[1].split('c')[0] * 1) - r;
+  }
+
+  return { x, y, r };
+}
+
+function flattenTranslateTransform(node) {
+  const transform = node.getAttribute('transform');
+  const coords = {x: 0, y: 0};
+
+  if (transform) {
+    const translateParams = transform.match(/translate\((\d+\.?\d+?)\,? (\d+\.?\d+)?\)/);
+    if (translateParams) {
+      coords.x = translateParams[1];
+      coords.y = translateParams[2];
+    }
+  }
+
+  Array.prototype.map.call(node.querySelectorAll('circle'), function(seat) {
+    const x = seat.getAttribute('cx');
+    const y = seat.getAttribute('cy');
+
+    if (x) {
+      seat.setAttribute('cx', (x * 1) + (coords.x * 1));
+    }
+
+    if (y) {
+      seat.setAttribute('cy', (y * 1) + (coords.y * 1));
+    }
+  });
+
+  node.removeAttribute('transform');
+}
+
+function removeClasses(svg) {
+  const seats = svg.querySelector('#plan-container').querySelectorAll('circle, path, polygon, rect');
+  if (seats) {
+    Array.prototype.forEach.call(seats, function(seat) {
+      seat.removeAttribute('class');
+    });
+  }
+}
+
+function removeIdTags(svg) {
+  const sectorShapes = svg.querySelectorAll('[id*="sector_shape"]');
+  Array.prototype.forEach.call(sectorShapes, function(sector) {
+    // var id = sector.getAttribute('id').replace(/\s?sector_shape\s?/, '');
+    sector.setAttribute('fill', 'lightgrey');
+    // sector.setAttribute('id', id);
+  });
+}
+
+function sortNodes(svg) {
+  const sectors = svg.getElementById('plan-container').children;
+
+  Array.prototype.forEach.call(sectors, function(sector) {
+    const rows = sector.querySelectorAll('g');
+
+    Array.prototype.forEach.call(rows, sortSeats);
+    sortRows(rows);
+  });
+}
+
+function sortRows(rows) {
+  if (rows.length === 0) {
+    return;
+  }
+
+  const arr = Array.prototype.slice.call(rows);
+  const parent = rows[0].parentNode;
+
+  if (arr.length === 0) {
+    return;
+  }
+
+  arr.sort(rowsCompare);
+
+  appendSortedNodes(arr, parent, function(row, idx) {
+    if (!row.getAttribute('tc-row-no')) {
+      row.setAttribute('tc-row-no', idx + 1);
+    }
+  });
+}
+
+function sortSeats(row) {
+  const seats = row.getElementsByTagName('circle');
+  if (!Array.prototype.slice.call(seats).length) {
+    const paths = row.getElementsByTagName('path');
+    Array.prototype.forEach.call(paths, function(path) {
+      const circle = convertPathToCircle(path);
+      path.parentNode.replaceChild(circle, path);
+    });
+  }
+
+  const arr = Array.prototype.slice.call(seats);
+
+  arr.sort(seatsCompare);
+
+  appendSortedNodes(arr, row, addSeatAttributes);
+}
+
+function rowsCompare(current, next) {
+  const currentNo = current.getAttribute('tc-row-no');
+  const nextNo = next.getAttribute('tc-row-no');
+  const currentRawHalfLength = Math.floor(current.getElementsByTagName('circle').length / 2);
+  const nextRawHalfLength = Math.floor(next.getElementsByTagName('circle').length / 2);
+  const currentY = current.getElementsByTagName('circle')[currentRawHalfLength].getAttribute('cy');
+  const nextY = next.getElementsByTagName('circle')[nextRawHalfLength].getAttribute('cy');
+
+  if (currentNo && nextNo) {
+    if (parseInt(currentNo, 10) > parseInt(nextNo, 10)) {
+      return 1;
+    } else if (parseInt(currentNo, 10) < parseInt(nextNo, 10)) {
+      return -1;
+    }
+    return 0;
+  }
+
+  if (currentY * 1 > nextY * 1) {
+    return 1;
+  } else if (currentY * 1 < nextY * 1) {
+    return -1;
+  }
+  return 0;
+}
+
+function seatsCompare(current, next) {
+  const currentNo = current.getAttribute('tc-seat-no');
+  const nextNo = next.getAttribute('tc-seat-no');
+
+  if (currentNo && nextNo) {
+    if (parseInt(currentNo, 10) > parseInt(nextNo, 10)) {
+      return 11;
+    } else if (parseInt(currentNo, 10) < parseInt(nextNo, 10)) {
+      return -1;
+    }
+    return 0;
+  }
+
+  if (current.getAttribute('cx') * 1 > next.getAttribute('cx') * 1) {
+    return -1;
+  } else if (current.getAttribute('cx') * 1 < next.getAttribute('cx') * 1) {
+    return 1;
+  }
+  return 0;
+}
+
+function appendSortedNodes(nodes, parent, callback) {
+  if (!nodes.forEach) {
+    nodes = Array.prototype.slice.call(nodes);
+  }
+
+  nodes.forEach(function(node, idx) {
+    let crlf = null;
+    if (callback && typeof callback === 'function') {
+      callback(node, idx);
+    }
+
+    if (node.nextSibling && node.nextSibling.nodeType === 3) {
+      crlf = node.nextSibling;
+    }
+
+    parent.appendChild(node);
+    if (crlf) {
+      parent.appendChild(crlf);
+    }
+  });
+}
+
+function addSeatAttributes(seat, idx) {
+  if (!seat.getAttribute('tc-seat-no')) {
+    seat.setAttribute('tc-seat-no', idx + 1);
+  }
+}
+
+function setInlineFont(font) {
+  return function(element) {
+    element.setAttribute('font-family', 'Arial');
+    if (font) {
+      element.setAttribute('font-size', font);
+    }
+  };
+}
+
+function setInlineStroke(stroke) {
+  return function(element) {
+    if (stroke) {
+      element.setAttribute('stroke', stroke);
+    }
+  };
+}
+
+function setInlineStrokeWidth(strokeWidth) {
+  return function(element) {
+    if (strokeWidth) {
+      element.setAttribute('stroke-width', strokeWidth);
+    }
+  };
+}
+
+function setSectorName(svg) {
+  var sectors = svg.getElementById('plan-container').children;
+
+  Array.prototype.forEach.call(sectors, function(sector) {
+    let sectorId = sector.getAttribute('id');
+
+    if (sector.getAttribute('tc-sector-name')) {
+      return;
+    }
+
+    if (sectorId) {
+      sectorId = sectorId.replace(/_(x(\d|[a|b|c|d|e]){4})_/g, function() {
+        return String.fromCharCode('0' + arguments[1]);
+      }).replace('-', ' ').trim();
+      sector.setAttribute('tc-sector-name', sectorId);
+    }
+  });
+}
+
+export {
+  cleanMeta,
+  wrapSingleGroups,
+  flattenStyles,
+  cleanTransforms,
+  convertPaths,
+  sortNodes,
+  setSectorName,
+  removeClasses,
+  removeIdTags,
+};
